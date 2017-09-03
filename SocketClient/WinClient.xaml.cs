bcb8d50d4325b819453547554918345e69d8a628
bananacheung <bananacheung@live.com>
2017-09-03 15:15:34 +0800

    添加项目文件。

---
+++ SocketClient/WinClient.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace SocketClient
+{
+    /// <summary>
+    ///     MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<IPAddress> _ipAddresses = new List<IPAddress>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GetLocalIpAddress();
+            LocalIpCombox.Items.Clear();
+            foreach (IPAddress ipAddress in _ipAddresses)
+            {
+                LocalIpCombox.Items.Add(ipAddress);
+            }
+            LocalIpCombox.SelectedIndex = 0;
+        }
+
+        private void GetLocalIpAddress()
+        {
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            // ReSharper disable once LoopCanBePartlyConvertedToQuery
+            foreach (NetworkInterface adapter in adapters)
+            {
+                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                UnicastIPAddressInformationCollection allAddress = adapterProperties.UnicastAddresses;
+
+                IPAddress[] ips = (from add in allAddress
+                                   where add.Address.AddressFamily == AddressFamily.InterNetwork
+                                   select add.Address).ToArray();
+
+                _ipAddresses.AddRange(ips);
+            }
+        }
+
+       
+    }
+}
---
+++ SocketServer/WinServer.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace SocketServer
+{
+    /// <summary>
+    ///     WinServer.xaml 的交互逻辑
+    /// </summary>
+    public partial class WinServer : Window
+    {
+        private readonly List<IPAddress> _ipAddresses = new List<IPAddress>();
+
+        public WinServer()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GetLocalIpAddress();
+            LocalIpCombox.Items.Clear();
+            foreach (IPAddress ipAddress in _ipAddresses)
+            {
+                LocalIpCombox.Items.Add(ipAddress);
+            }
+            LocalIpCombox.SelectedIndex = 0;
+        }
+
+        private void GetLocalIpAddress()
+        {
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            // ReSharper disable once LoopCanBePartlyConvertedToQuery
+            foreach (NetworkInterface adapter in adapters)
+            {
+                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                UnicastIPAddressInformationCollection allAddress = adapterProperties.UnicastAddresses;
+
+                IPAddress[] ips = (from add in allAddress
+                                   where add.Address.AddressFamily == AddressFamily.InterNetwork
+                                   select add.Address).ToArray();
+
+                _ipAddresses.AddRange(ips);
+            }
+        }
+    }
+}
